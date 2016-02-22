9ca3cba3ef81bd62b4478d73091a00d7051562ea
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-22 19:45:29 -0300

    Some method example on JSDoc

--- src/utils/some.js
+++ src/utils/some.js
@@ -6,6 +6,18 @@
  * It should return a truthy value in order to stop the loop.
  * @param {Number} [index] - A starting index. Default is 0.
  * @throws {TypeError} Will throw a TypeError if no callback function was provided.
+ * @example
+ * let harr = new Harray(1, 3);
+ * let oddsUntilTen = [];
+ * harr.some(function(element) {
+ *      if (element >= 10) {
+ *          return true;
+ *      } else {
+ *          oddsUntilTen.push(element);
+ *      }
+ * });
+ *
+ * console.log(oddsUntilTen) // -> [1, 3, 5, 7, 9]
  */
 export function some(fn, index) {
     index = typeof index === 'number' ? index : 0;
