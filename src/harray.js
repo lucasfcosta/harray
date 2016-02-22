d92581bc8e083f2433d1a718806b193af9e46a7d
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-21 21:09:09 -0300

    Harray class main file documentation

--- src/harray.js
+++ src/harray.js
@@ -6,6 +6,15 @@ import { some } from './utils/some';
 import { every } from './utils/every';
 import { getReadableStream } from './utils/getReadableStream';
 
+/**
+ * @class Harray
+ * @classdesc An infinite array.
+ * @property {Infinity} length - Returns the length of the Harray. (TIP: It's infinite)
+ * @param {...Number} element - Every number passed as argument before the formula will be used as an element.
+ * @param {Function} formula - A formula which will be used to calculate the next element. If it does not exist
+ * the difference between the last two elements will be used as increment value to generate the sequence.
+ * If there's a single element and no formula was provided the sequence will be generated using 1 as increment.
+ */
 function Harray() {
     let args = arguments;
 
@@ -31,6 +40,15 @@ function Harray() {
     }
 }
 
+Harray.prototype.length = Infinity;
+
+/**
+ * Gets the value from an index.
+ * @method
+ * @name Harray#get
+ * @param {Number} index - A value's index.
+ * @returns value - The value of an index.
+ */
 Harray.prototype.get = function get(index) {
     if (this[index] !== undefined) {
         return this[index];
@@ -47,8 +65,13 @@ Harray.prototype.get = function get(index) {
     }
 };
 
-Harray.prototype.length = Infinity;
-
+/**
+ * Adds a method to the Harray prototype.
+ * @method
+ * @name Harray.addMethod
+ * @param {String} methodName - The name of the property which will hold the method.
+ * @param {Function} method - The method which will be added to the prototype.
+ */
 Harray.addMethod = function addMethod(methodName, method) {
     Harray.prototype[methodName] = method;
 };
