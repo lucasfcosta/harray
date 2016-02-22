64156eedb671fea75ffd40b6b7ea12456376b223
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-22 19:42:57 -0300

    every method example on JSDoc

--- src/utils/every.js
+++ src/utils/every.js
@@ -6,6 +6,19 @@
  * It should return a falsy value in order to stop the loop.
  * @param {Number} [index] - A starting index. Default is 0.
  * @throws {TypeError} Will throw a TypeError if no callback function was provided.
+ * @example
+ * let harr = new Harray(0, 2);
+ * let evensUntilTen = [];
+ * harr.every(function(element) {
+ *      if (element >= 10) {
+ *          return false;
+ *      } else {
+ *          evensUntilTen.push(element);
+ *          return true;
+ *      }
+ * });
+ *
+ * console.log(evensUntilTen) // -> [0, 2, 4, 6, 8]
  */
 export function every(fn, index) {
     index = typeof index === 'number' ? index : 0;
