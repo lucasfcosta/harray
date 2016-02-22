7d3f33aef23af7f9c86edc09a53d0e60e7bd779c
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-22 18:24:03 -0300

    getRange method documentation

--- src/utils/getRange.js
+++ src/utils/getRange.js
@@ -1,3 +1,13 @@
+/**
+ * Gets an aray with numbers from a start index until a final index (inclusively).
+ * If the final index is smaller then the initial index the returned array will be in the reverse order (from start to end, as expected).
+ * If only one argument is passed it will be used as the final index.
+ * @name Harray#getRange
+ * @method
+ * @param {Number} [start] - The initial index. Defaults to 0.
+ * @param {Number} [end] - The final index. Defaults to 0.
+ * @returns rangeArray - An array of numbers from the initial index until the final index (inclusively).
+ */
 export function getRange(start, end) {
     start = typeof start === 'number' ? start : 0;
     end = typeof end === 'number' ? end : 0;
