d3b7fab2e559599629c11578b3c841fc5f2670e8
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-22 18:24:35 -0300

    some method documentation

--- src/utils/some.js
+++ src/utils/some.js
@@ -1,3 +1,12 @@
+/**
+ * This method calls the callback function once for every element in the Harray until it returns a truthy value.
+ * @name Harray#some
+ * @method
+ * @param {Function} fn - A callback function which will be called with the element as the only argument.
+ * It should return a truthy value in order to stop the loop.
+ * @param {Number} [index] - A starting index. Default is 0.
+ * @throws {TypeError} Will throw a TypeError if no callback function was provided.
+ */
 export function some(fn, index) {
     index = typeof index === 'number' ? index : 0;
 
