cc3f87de741139e88fb1b15d045ded7ca7066d55
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-24 18:15:49 -0300

    Add tests to cover starting index into every and some methods

--- test/harraySomeTests.js
+++ test/harraySomeTests.js
@@ -20,6 +20,23 @@ describe('Some Method', () => {
         assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
     });
 
+    it('Accepts an starting index', () => {
+        let h = new Harray(0, 2);
+        let expected = [4, 6, 8, 10];
+        let actual = [];
+
+        h.some(function(el) {
+            if (el > 10) {
+                return false;
+            } else {
+                actual.push(el);
+                return true;
+            }
+        }, 2);
+
+        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+    });
+
     it('Throws a TypeError if the argument is not an array', () => {
         let h = new Harray(0, 2);
         assert.throws(() => {
